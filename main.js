1902ae75925afc3f169027a2d67f3669daf17979
Pietro Giordano <giopi87@gmail.com>
2023-01-18 16:02:48 +0100

    milestone 2

--- main.js
+++ main.js
@@ -55,3 +55,43 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+const container = document.getElementById('container');
+
+posts.forEach((post) => {
+
+    const newPost = document.createElement('div');
+    newPost.classList.add('post');
+    newPost.innerHTML = `
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src=${post.author.image} alt="${post.author.name}">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${post.author.name}</div>
+                    <div class="post-meta__time">${post.created}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${post.content}</div>
+        <div class="post__image">
+            <img src=${post.media} alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    `;
+    container.append(newPost);
+
+});
