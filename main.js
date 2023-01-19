936f501bdd2c5db760046207a0e86d77259aafd5
Pietro Giordano <giopi87@gmail.com>
2023-01-19 10:52:56 +0100

    aggiornamento

--- main.js
+++ main.js
@@ -60,39 +60,38 @@ const container = document.getElementById('container');
 
 posts.forEach((post) => {
 
-    const newPost = document.createElement('div');
-    newPost.classList.add('post');
-    newPost.innerHTML = `
-        <div class="post__header">
-            <div class="post-meta">                    
-                <div class="post-meta__icon">
-                    <img class="profile-pic" src=${post.author.image} alt="${post.author.name}">                    
+   container.innerHTML += `
+        <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src=${post.author.image} alt="${post.author.name}">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${post.author.name}</div>
+                        <div class="post-meta__time">${post.created}</div>
+                    </div>                    
                 </div>
-                <div class="post-meta__data">
-                    <div class="post-meta__author">${post.author.name}</div>
-                    <div class="post-meta__time">${post.created}</div>
-                </div>                    
             </div>
+            <div class="post__text">${post.content}</div>
+            <div class="post__image">
+                <img src=${post.media} alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid=${post.id}>
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
         </div>
-        <div class="post__text">${post.content}</div>
-        <div class="post__image">
-            <img src=${post.media} alt="">
-        </div>
-        <div class="post__footer">
-            <div class="likes js-likes">
-                <div class="likes__cta">
-                    <a class="like-button  js-like-button" href="##" data-postid=${post.id}>
-                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                        <span class="like-button__label">Mi Piace</span>
-                    </a>
-                </div>
-                <div class="likes__counter">
-                    Piace a <b id="like-counter-1" class="js-likes-counter">${post.likes}</b> persone
-                </div>
-            </div> 
-        </div>            
     `;
-    container.append(newPost);
 
 });
 
@@ -105,9 +104,10 @@ buttonsLike.forEach((singleButton, index) => {
 
     singleButton.addEventListener('click',
     
-        () => {
+        function(event) {
 
             console.log(singleButton);
+            event.preventDefault();
 
             if(singleButton.matches('.like-button--liked')) {
 
